948096a153f1696daa0900fb957568f9f57a6993
DHARANI471 <147544070+DHARANI471@users.noreply.github.com>
2026-01-17 16:34:45 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -47,7 +47,7 @@ function Register() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("https://unimart-backend-r87y.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -106,3 +106,4 @@ function Register() {
 }
 
 export default Register;
+
